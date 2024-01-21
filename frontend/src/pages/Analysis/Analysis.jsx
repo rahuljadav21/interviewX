4c44cbaf9286d6543f0ebc2f9e6d93ed8c8163ac
rahuljadav21 <rahuljadav2121@gmail.com>
2024-01-21 23:08:39 +0530

    title added to ifram

--- frontend/src/pages/Analysis/Analysis.jsx
+++ frontend/src/pages/Analysis/Analysis.jsx
@@ -7,6 +7,7 @@ function About() {
       <h1 style={{ color: "#279eff" }}>ECE '24 Analysis</h1>
       <p style={{ fontSize: "larger" }}>
         <iframe
+          title="Placement Analysis"
           scrolling="no"
           className="sheet"
           src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTHnpz49dNy88ComgYEmqNfsFxWbMHsAy0R8gLn34A08yPzcXr3zJYfZEqx9wqHjgciLMsE3gF8ykSW/pubhtml?gid=1768645801&amp;single=true&amp;widget=true&amp;headers=false"
